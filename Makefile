b0a608319ce61291f9b5df607d4ceb3cac058eaa
lalithkumar24 <lali22048.it@rmkec.ac.in>
2025-03-11 16:04:24 +0000

    implmented makefile,gitignore and organized code

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 CC=clang
 CFLAGS=-Wall -Wextra -pedantic -std=c99 -ggdb -Werror=return-type
 
-server: server.c
-		${CC} ${CFLAGS} $^ -o $@
+server: server.c string_operations.c string_operations.h
+	${CC} ${CFLAGS} server.c string_operations.c -o $@
 
 .PHONY: clean
-
 clean:
-		rm -f server
+	rm -f server
--- string_operations.c
+++ string_operations.c
@@ -30,7 +30,7 @@ string_splits split_string(const char *str, size_t len, const char *split_by) {
   size_t split_by_len = strlen(split_by);
 
   result.capacity = 8;
-  result.splits = calloc(sizeof(string_view), result.capacity);
+  result.splits =(string_view*) calloc(sizeof(string_view), result.capacity);
   result.count = 0;
   for (size_t i = 0; i < len; ++i) {
     if (i + split_by_len < len &&
@@ -43,8 +43,8 @@ string_splits split_string(const char *str, size_t len, const char *split_by) {
       i += split_by_len;
       if (result.count == result.capacity) {
         result.capacity *= 2;
-        string_view *temp =
-            realloc(result.splits, sizeof(string_view) * result.capacity);
+        string_view *temp = (string_view *)realloc(
+            result.splits, sizeof(string_view) * result.capacity);
         if (temp) {
           result.splits = temp;
         } else {
